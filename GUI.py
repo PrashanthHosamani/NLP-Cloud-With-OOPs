5036f8a6a757d83182324b0623faae52a25c07f5
PrashanthHosamani <prashanthprashanth6753@gmail.com>
2025-09-20 23:30:44 +0530

    update

---
+++ GUI.py
@@ -0,0 +1,16 @@
+from tkinter import *
+class NLPApp:
+    
+    def __init__(self):
+    
+    #login ka GUI 
+        self.root = Tk() #created GUI
+        self.root.title('NLP APP')
+        self.root.iconbitmap('resources/favicon.ico')
+        self.root.mainloop() #kept on the screen 
+    
+
+nlp = NLPApp()
+
+
+nlp = NLPApp()
